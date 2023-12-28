2536dead5e7e99d0dcd8d3af28b5df00d9167a93
rajahwu <vmradford@gmail.com>
2023-12-28 11:32:42 -0500

    add default form styles

--- react-app/src/pages/ProfilePage/index.tsx
+++ react-app/src/pages/ProfilePage/index.tsx
@@ -1,10 +1,14 @@
 export default function ProfilePage() {
   return (
-    <form>
-      <label>username</label>
-      <input />
-      <label>email</label>
-      <input />
+    <form className="flex flex-col">
+      <div>
+        <label htmlFor="username">username</label>
+        <input type="text" name="username" id="username" />
+      </div>
+      <div>
+        <label htmlFor="email">email</label>
+        <input type="email" name="email" id="email" />
+      </div>
       <input type="submit" />
     </form>
   );
