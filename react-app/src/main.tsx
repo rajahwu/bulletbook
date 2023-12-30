cc39b180b7b5ad60d059a0e1e29d89b39314ac61
rajahwu <vmradford@gmail.com>
2023-12-30 14:01:00 -0500

    add project page route

--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -1,62 +1,75 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import {
   Route,
   RouterProvider,
   createBrowserRouter,
-  createRoutesFromElements
-} from 'react-router-dom';
+  createRoutesFromElements,
+} from "react-router-dom";
 
-import { ProfileAction } from './data/router/actions';
-import { ProfileLoader, TechLoader, TechViewLoader } from './data/router/loaders';
-import './index.css';
+import { ProfileAction } from "./data/router/actions";
+import {
+  ProfileLoader,
+  TechLoader,
+  TechViewLoader,
+} from "./data/router/loaders";
+import "./index.css";
 import { Root } from "./layouts";
-import { ErrorPage, LandingPage, ProfilePage, TechPage, TechViewPage } from './pages';
-
+import {
+  ErrorPage,
+  LandingPage,
+  ProfilePage,
+  ProjectPage,
+  TechPage,
+  TechViewPage,
+} from "./pages";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-  <Route
-  path="/"
-  element={<Root />}
-  errorElement={<ErrorPage />}
-  >
-    <Route 
-    path="/home"
-    element={<LandingPage />} 
-    errorElement={<ErrorPage />}
-    />
+    <Route path="/" element={<Root />} errorElement={<ErrorPage />}>
+      <Route
+        path="/home"
+        element={<LandingPage />}
+        errorElement={<ErrorPage />}
+      />
 
-    <Route 
-    path="/profile"
-    element={<ProfilePage />}
-    errorElement={<ErrorPage />}
-    loader={ProfileLoader}
-    action={ProfileAction}
-    />
+      <Route
+        path="/profile"
+        element={<ProfilePage />}
+        errorElement={<ErrorPage />}
+        loader={ProfileLoader}
+        action={ProfileAction}
+      />
 
-    <Route 
-    path="/technologies"
-    element={<TechPage />} 
-    errorElement={<ErrorPage />}
-    loader={TechLoader}
-    />
+      <Route
+        path="/technologies"
+        element={<TechPage />}
+        errorElement={<ErrorPage />}
+        loader={TechLoader}
+      />
 
-    <Route
-    path="/technologies/*"
-    element={<TechViewPage />} 
-    errorElement={<ErrorPage />}
-    loader={TechViewLoader}
-    />
+      <Route
+        path="/technologies/*"
+        element={<TechViewPage />}
+        errorElement={<ErrorPage />}
+        loader={TechViewLoader}
+      />
 
-  <Route errorElement={<div>Page Not Found</div>} />
+      <Route
+        path="/projects"
+        element={<ProjectPage />}
+        errorElement={<ErrorPage />}
+      >
+        <Route path="/projects/:id" element={<ProjectPage />} />
+      </Route>
 
-  </Route>
+      <Route errorElement={<div>Page Not Found</div>} />
+    </Route>
   )
 );
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- react-app/src/pages/index.ts
+++ react-app/src/pages/index.ts
@@ -1,13 +1,12 @@
+import ErrorPage from "./ErrorPage"
 import LandingPage from "./LandingPage"
 import ProfilePage from "./ProfilePage"
+import ProjectPage from "./ProjectPage"
 import TechPage from "./TechPage"
 import TechViewPage from "./TechViewPage"
-import ErrorPage from "./ErrorPage"
 
 export {
-    LandingPage,
-    ProfilePage,
-    TechPage,
-    TechViewPage,
-    ErrorPage
+    ErrorPage, LandingPage,
+    ProfilePage, ProjectPage, TechPage,
+    TechViewPage
 }
